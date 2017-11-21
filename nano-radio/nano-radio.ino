e8258871cd9d79c342b87fed0982e6a3a6561ac1
tarasii <tarasii@gmail.com>
2017-11-21 22:15:33 +0200

    encoder added

--- nano-radio/nano-radio.ino
+++ nano-radio/nano-radio.ino
@@ -13,6 +13,10 @@
 #define PCD8544_RST 2
 #define PCD8544_LIGHT 7
 
+#define ENC_A 9
+#define ENC_B 8
+#define ENC_C 10
+
 #define LED 13
 
 #define BTN_CNTR 678
@@ -48,6 +52,11 @@ uint16_t tmp_btn;
 int8_t cur_vol = MAX_VOL;
 uint8_t cur_cmd = 0;
 
+uint32_t cur_mil, tmp_mil;
+
+int8_t cnt_enc = 0;
+uint8_t tmp_enc;
+
 //void printBuf(uint8_t *buf, uint8_t len){
 //  uint8_t i, m, n;
 //  for (i=0;i<len;i++){ 
@@ -69,12 +78,6 @@ uint8_t cur_cmd = 0;
 //}
 
 void setup() {
-  byte res;
-
-  int i;
-
-
-  //memset(buf,0,30);
   
   Serial.begin(9600);
   Serial.println("Start"); 
@@ -82,6 +85,10 @@ void setup() {
   pinMode(LED, OUTPUT);
   pinMode(PCD8544_LIGHT, OUTPUT);
 
+  pinMode (ENC_A, INPUT);
+  pinMode (ENC_B, INPUT);
+  pinMode (ENC_C, INPUT);
+  
   digitalWrite(PCD8544_LIGHT, HIGH);
       
   display.begin(84, 48);              // Инициализация дисплея
@@ -93,7 +100,8 @@ void setup() {
   display.setCursor(0, 0);
   display.print("hello");
   delay(500);
-
+  
+  tmp_mil = millis();
 
   cl5767.init();
   cl5767.print_state();
@@ -101,7 +109,6 @@ void setup() {
   cl5767.turn_on();
   cl5767.set_vol(cur_vol);
   cl5767.tune(sttns[cur_sttn]);
-
 
   cl5767.read_buf();  
   cl5767.print_state_hex();
@@ -113,88 +120,30 @@ void setup() {
   
   //display.setBacklight(true);
 
+  tmp_enc = digitalRead(ENC_A);
 
 }
 
 void loop() {
   uint8_t i;
   char tmp;
+  uint8_t cur_enc, zzz;
   uint16_t cur_btn;
 
-  
-  RTC.readTime();
-
   cur_btn = analogRead(A0);
-  
-//  Serial.print(RTC.yyyy, DEC); 
-//  Serial.print("-"); 
-//  Serial.print(RTC.mm, DEC); 
-//  Serial.print("-"); 
-//  Serial.print(RTC.dd, DEC);
-//  Serial.print(" "); 
-//  Serial.print(RTC.h, DEC);
-//  Serial.print(":"); 
-//  Serial.print(RTC.m, DEC);
-//  Serial.print(":"); 
-//  Serial.print(RTC.s, DEC);
-//  Serial.println("");
 
-  display.setCursor(0, 0);
-  display.print(RTC.yyyy, DEC); 
-  display.print("-");
-  if (RTC.mm < 10) display.print(0);
-  display.print(RTC.mm, DEC); 
-  display.print("-"); 
-  if (RTC.dd < 10) display.print(0);
-  display.print(RTC.dd, DEC);
-  display.setCursor(0, 1);
-  if (RTC.h < 10) display.print(0);
-  display.print(RTC.h, DEC);
-  display.print(":"); 
-  if (RTC.m < 10) display.print(0);
-  display.print(RTC.m, DEC);
-  display.print(":"); 
-  if (RTC.s < 10) display.print(0);
-  display.print(RTC.s, DEC);
+  cur_enc = digitalRead(ENC_A);
+  if (cur_enc != tmp_enc) {
+    if (digitalRead(ENC_B) != cur_enc) {
+      cnt_enc++; 
+      cur_cmd = CMD_VOL_UP; 
+    } else {
+      cnt_enc--;
+      cur_cmd = CMD_VOL_DOWN;
+    }
 
-
-  cl5767.read_status();
-  //cl5767.print_state_hex();
-  
-  display.setCursor(0, 2);
-  display.print("freq:");
-  display.print(cl5767.get_freq());
-  //display.print(tmp);
-  display.print(" ");
-
-  display.setCursor(0, 3);
-  display.print("vol:");
-  display.print(cl5767.get_vol());
-  if (cl5767.get_stereo()) display.print(" STEREO");
-  else display.print(" MONO   "); 
-
-  display.setCursor(0, 4);
-  display.print("rssi:");
-  display.print(cl5767.get_rssi());
-  display.print(" e:");
-  display.print(cl5767.get_envlp_num());
-  display.print("  ");
-          
-  if (blnk) {
-    digitalWrite(LED, HIGH);
-    blnk = false;
+    tmp_enc = cur_enc;
   }
-  else {
-    digitalWrite(LED, LOW);
-    blnk = true;
-  }
-
-  delay(500); 
-
-  display.setCursor(0, 5);  
-  display.print("btn:");
-  display.print(cur_btn);
-  display.print("  ");
 
   if (Serial.available()){
     tmp = Serial.read();
@@ -247,41 +196,123 @@ void loop() {
       cur_cmd = CMD_VOL_UP;
     }
   }
+  
+  cur_mil = millis();
+  if (cur_mil - tmp_mil > 500){
+    tmp_mil = cur_mil;
+      
+    RTC.readTime();
+      
+    display.setCursor(0, 0);
+    display.print(RTC.yyyy, DEC); 
+    display.print("-");
+    if (RTC.mm < 10) display.print(0);
+    display.print(RTC.mm, DEC); 
+    display.print("-"); 
+    if (RTC.dd < 10) display.print(0);
+    display.print(RTC.dd, DEC);
+    display.setCursor(0, 1);
+    if (RTC.h < 10) display.print(0);
+    display.print(RTC.h, DEC);
+    display.print(":"); 
+    if (RTC.m < 10) display.print(0);
+    display.print(RTC.m, DEC);
+    display.print(":"); 
+    if (RTC.s < 10) display.print(0);
+    display.print(RTC.s, DEC);
 
-  switch (cur_cmd){
-    case CMD_CHAN_DOWN:
-      if (--cur_sttn<0) cur_sttn=7; 
-      cl5767.tune(sttns[cur_sttn]);
-      break;
-    case CMD_CHAN_UP:
-      if (++cur_sttn>=8) cur_sttn=0;
-      cl5767.tune(sttns[cur_sttn]);
-      break;
-    case CMD_SEEK_DOWN:
-      cl5767.seek_chanel(false);
-      break;
-    case CMD_SEEK_UP: 
-      cl5767.seek_chanel(true);
-      break;
-    case CMD_VOL_DOWN:
-      if (--cur_vol<0) cur_vol=0;
-      cl5767.set_vol(cur_vol);
-      cl5767.write_func();
-      break;
-    case CMD_VOL_UP:
-      if (++cur_vol>=MAX_VOL) cur_vol=MAX_VOL;
-      cl5767.set_vol(cur_vol);
-      cl5767.write_func();
-      break;    
-  }
-  if (cur_cmd!=0){
+
     cl5767.read_status();
-    cl5767.print_state_hex();
-    Serial.print("cmd:");
-    Serial.println(cur_cmd);
-    cl5767.print_state();
+    //cl5767.print_state_hex();
     
-    cur_cmd = 0;
+    display.setCursor(0, 2);
+    display.print("freq:");
+    display.print(cl5767.get_freq());
+    //display.print(tmp);
+    display.print(" ");
+  
+    display.setCursor(0, 3);
+    display.print("vol:");
+    display.print(cl5767.get_vol());
+    if (cl5767.get_stereo()) display.print(" STEREO");
+    else display.print(" MONO   "); 
+  
+    display.setCursor(0, 4);
+    display.print("rssi:");
+    display.print(cl5767.get_rssi());
+    display.print(" e:");
+    display.print(cl5767.get_envlp_num());
+    display.print("  ");
+            
+    if (blnk) {
+      digitalWrite(LED, HIGH);
+      blnk = false;
+    }
+    else {
+      digitalWrite(LED, LOW);
+      blnk = true;
+    }
+  
+    display.setCursor(0, 5);  
+    display.print("btn:");
+    display.print(cur_btn);
+    display.print("  ");
+    display.print("enc:");
+    display.print(cnt_enc);
+    display.print("   ");
+
+    switch (cur_cmd){
+      case CMD_CHAN_DOWN:
+        if (--cur_sttn<0) cur_sttn=7; 
+        cl5767.tune(sttns[cur_sttn]);
+        break;
+      case CMD_CHAN_UP:
+        if (++cur_sttn>=8) cur_sttn=0;
+        cl5767.tune(sttns[cur_sttn]);
+        break;
+      case CMD_SEEK_DOWN:
+        cl5767.seek_chanel(false);
+        break;
+      case CMD_SEEK_UP: 
+        cl5767.seek_chanel(true);
+        break;
+      case CMD_VOL_DOWN:
+        if (--cur_vol<0) cur_vol=0;
+        cl5767.set_vol(cur_vol);
+        cl5767.write_func();
+        break;
+      case CMD_VOL_UP:
+        if (++cur_vol>=MAX_VOL) cur_vol=MAX_VOL;
+        cl5767.set_vol(cur_vol);
+        cl5767.write_func();
+        break;    
+    }
+    if (cur_cmd!=0){
+      cl5767.read_status();
+      cl5767.print_state_hex();
+      Serial.print("cmd:");
+      Serial.println(cur_cmd);
+      cl5767.print_state();
+
+      Serial.print(RTC.yyyy, DEC); 
+      Serial.print("-"); 
+      Serial.print(RTC.mm, DEC); 
+      Serial.print("-"); 
+      Serial.print(RTC.dd, DEC);
+      Serial.print(" "); 
+      Serial.print(RTC.h, DEC);
+      Serial.print(":"); 
+      Serial.print(RTC.m, DEC);
+      Serial.print(":"); 
+      Serial.print(RTC.s, DEC);
+      Serial.println("");  
+          
+      cur_cmd = 0;
+    }    
+
   }
+
+
+
 
 }
